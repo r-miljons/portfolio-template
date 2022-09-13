554a62b1f748f238c54a0056e4c69998d5138a99
Renars Miljons <reku.miljons@gmail.com>
2022-09-13 17:15:27 +0300

    updated schema

--- src/pages/Thankyou.js
+++ src/pages/Thankyou.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client';
+import { PortableText } from '@portabletext/react';
+
+const FORM_MESSAGE = gql`
+query {
+	allContact {
+    messageSentTitle,
+    messageSentRaw
+  }
+}
+`
 
 export default function Thankyou() {
+  const { data, loading, error } = useQuery(FORM_MESSAGE);
+
   const navigate = useNavigate();
+
+  if (loading) return <p style={{textAlign: 'center'}}>Loading...</p>
+  if (error) return <p style={{textAlign: 'center'}}>Error</p>
+
+    const message = data.allContact[0]
 
   return (
     <div className='thankyou-container'>
         <div className='thankyou-message'>
-            <h1>Thank You!</h1>
-            <p>Your message has been sent and I will get back to you as soon as possible.</p>
+            <h1>{message.messageSentTitle}</h1>
+            <PortableText value={message.messageSentRaw}/>
             <button className='main-button' onClick={() => navigate("/")}>Close</button>
         </div>
     </div>
--- studio/schemas/contact.js
+++ studio/schemas/contact.js
@@ -21,6 +21,18 @@ export default {
             title: "Contact Email",
             description: "Emails will be sent to this address when a user sends the contact form. Send a test email by filling out the form to activate it and to make sure everything works as expected!",
             validation: Rule => Rule.required(),
+        },
+        {
+            name: "messageSentTitle",
+            type: "string",
+            title: "Message sent title",
+            description: "This will appear as a confirmation message to the user when the contact form was submitted successfully"
+        },
+        {
+            name: "messageSent",
+            type: "richText",
+            title: "Message sent",
+            description: "This will appear as a confirmation message to the user when the contact form was submitted successfully"
         }
     ]
 }
